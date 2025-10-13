b30044b2ad98a35120a1da2b7030c93454f1da7a
DangKunpeng <kunpeng.dang@accenture.cn>
2025-10-13 09:31:24 +0800

    #msg

---
+++ sam_commons/src/main/java/com/sam/sap_commons/configs/MdcRabbitHandlerAspect.java
@@ -0,0 +1,28 @@
+package com.sam.sap_commons.configs;
+
+import com.sam.sap_commons.utils.AjaxUtils;
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.amqp.rabbit.annotation.RabbitHandler;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Aspect
+@Component
+public class MdcRabbitHandlerAspect {
+
+    // 拦截所有带有 @RabbitHandler 注解的方法
+    @Around("@annotation(rabbitHandler)")
+    public Object aroundRabbitHandler(ProceedingJoinPoint joinPoint, RabbitHandler rabbitHandler) throws Throwable {
+        // 切面前置逻辑
+        log.info("RabbitHandler 方法被调用: {}", joinPoint.getSignature());
+        // 执行业务方法
+        Object result = joinPoint.proceed();
+        // 切面后置逻辑
+        log.info("RabbitHandler 方法执行完毕");
+        AjaxUtils.removeTraceId();
+        return result;
+    }
+}
--- sam_commons/src/main/java/com/sam/sap_commons/mq/MqCommonProducer.java
+++ sam_commons/src/main/java/com/sam/sap_commons/mq/MqCommonProducer.java
@@ -3,9 +3,9 @@ package com.sam.sap_commons.mq;
 import com.sam.sap_commons.exchange.ApiMq;
 import com.sam.sap_commons.utils.AjaxUtils;
 import com.sam.sap_commons.utils.JsonUtil;
+import com.sam.sap_commons.utils.SysDefaults;
 import jakarta.annotation.Resource;
 import lombok.extern.slf4j.Slf4j;
-import org.slf4j.MDC;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.stereotype.Service;
 
@@ -15,10 +15,23 @@ public class MqCommonProducer {
     @Resource
     private RabbitTemplate rabbitTemplate;
 
-    public void sendMsg(ApiMq msg) {
+    public void sendMsg(String queue, String mqBody) {
         // 设置消息对象里的traceId
-        msg.setTraceId(AjaxUtils.getTraceId());
-        log.info("MQ: sending msg {}", msg);
-        this.rabbitTemplate.convertAndSend(msg.getMqType(), JsonUtil.toJsonString(msg));
+        ApiMq apiMq = ApiMq.builder()
+                .mqType(queue)
+                .mqBody(mqBody)
+                .timestamp(SysDefaults.now())
+                .build();
+        apiMq.setTraceId(AjaxUtils.getTraceId());
+        this.sendMsg(apiMq);
+    }
+
+    public void sendMsg(ApiMq apiMq) {
+        // 设置消息对象里的traceId
+        apiMq.setTraceId(AjaxUtils.getTraceId());
+        log.info("MQ: sending msg {}", apiMq);
+        this.rabbitTemplate.convertAndSend(apiMq.getMqType(), JsonUtil.toJsonString(apiMq));
+        // 移除
+        AjaxUtils.removeTraceId();
     }
 }
