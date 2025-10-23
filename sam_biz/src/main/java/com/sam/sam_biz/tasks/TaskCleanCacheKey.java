83c8a64997d1a18039846ee80114cf2eab170bad
DangKunpeng <kunpeng.dang@accenture.cn>
2025-10-23 16:42:23 +0800

    #redis cache

---
+++ sam_biz/src/main/java/com/sam/sam_biz/tasks/TaskCleanCacheKey.java
@@ -0,0 +1,40 @@
+package com.sam.sam_biz.tasks;
+
+import com.sam.sap_commons.utils.FmtUtils;
+import com.sam.sap_commons.utils.SysDefaults;
+import jakarta.annotation.Resource;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.collections4.CollectionUtils;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+import static com.sam.sap_commons.utils.SysDefaults.SYS_DEFAULT_DAY_PATTERN;
+
+@Slf4j
+@Service
+public class TaskCleanCacheKey {
+    private static final String KEY_PATTERN = "*{}*";
+    @Resource
+    private StringRedisTemplate stringRedisTemplate;
+
+
+    @Async
+    @Scheduled(cron = "0 0 * * * ?")
+    public void run() {
+        // 查询key
+        String key = FmtUtils.fmtMsg(KEY_PATTERN, SysDefaults.minusDays(1, SYS_DEFAULT_DAY_PATTERN));
+        this.deleteByPattern(key);
+        log.info("deleteByPattern {}", key);
+    }
+
+    public void deleteByPattern(String pattern) {
+        Set<String> keys = this.stringRedisTemplate.keys(pattern);
+        if (CollectionUtils.isNotEmpty(keys)) {
+            this.stringRedisTemplate.delete(keys);
+        }
+    }
+}
--- sam_biz/src/main/java/com/sam/sam_biz/tasks/TaskQueueInCache.java
+++ sam_biz/src/main/java/com/sam/sam_biz/tasks/TaskQueueInCache.java
@@ -1,53 +1,33 @@
 package com.sam.sam_biz.tasks;
 
 import com.sam.sap_commons.redis.RedisCacheHelper;
-import com.sam.sap_commons.utils.FmtUtils;
-import com.sam.sap_commons.utils.SysDefaults;
 import jakarta.annotation.Resource;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.collections4.CollectionUtils;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
-import java.util.Set;
-
-import static com.sam.sap_commons.utils.SysDefaults.SYS_DEFAULT_DAY_PATTERN;
-
 @Slf4j
 @Service
-public class TaskCleanCache {
-    private static final String KEY_PATTERN = "*{}*";
+public class TaskQueueInCache {
     private static final String KEY_TASK = "TaskCleanCache";
     @Resource
     private StringRedisTemplate stringRedisTemplate;
 
 
     @Async
-    @Scheduled(cron = "0/5 * * * * ?")
+    @Scheduled(cron = "0/2 * * * * ?")
     public void run() {
         RedisCacheHelper.leftPush(KEY_TASK, RedisCacheHelper.newKey(KEY_TASK));
         log.info("task {} started, size = {}", Thread.currentThread().getName(), stringRedisTemplate.opsForList().size(KEY_TASK));
-
-        try {
-            Thread.sleep(16000L);
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        }
-        // 查询key
-        String key = FmtUtils.fmtMsg(KEY_PATTERN, SysDefaults.minusDays(1, SYS_DEFAULT_DAY_PATTERN));
-        this.deleteByPattern(key);
     }
 
-
-    public void deleteByPattern(String pattern) {
+    @Async
+    @Scheduled(cron = "0/5 * * * * ?")
+    public void execute() {
         RedisCacheHelper.rightPop(KEY_TASK);
         log.info("task {} done, size = {}", Thread.currentThread().getName(), stringRedisTemplate.opsForList().size(KEY_TASK));
-        Set<String> keys = this.stringRedisTemplate.keys(pattern);
-        if (CollectionUtils.isEmpty(keys)) {
-            return;
-        }
-        this.stringRedisTemplate.delete(keys);
+
     }
 }
