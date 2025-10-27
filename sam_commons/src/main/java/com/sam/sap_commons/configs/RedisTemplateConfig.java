b217903cedd9c5cc6ccbefd3270524739fa09c64
DangKunpeng <kunpeng.dang@accenture.cn>
2025-10-27 14:19:22 +0800

    #redis cache

--- sam_commons/src/main/java/com/sam/sap_commons/configs/RedisTemplateConfig.java
+++ sam_commons/src/main/java/com/sam/sap_commons/configs/RedisTemplateConfig.java
@@ -9,7 +9,7 @@ import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSeriali
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
-public class RedisConfig {
+public class RedisTemplateConfig {
     @Bean
     public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory factory) {
         RedisTemplate<String, Object> template = new RedisTemplate<>();
